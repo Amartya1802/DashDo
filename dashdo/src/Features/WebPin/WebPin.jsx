f6090eded35a991be6d922c6ca7315b8f5a437a1
Adarsh <adarsh.gupta.mat20@itbhu.ac.in>
2023-07-14 23:58:30 +0530

    WebPin part

---
+++ dashdo/src/Features/WebPin/WebPin.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { getAuth, onAuthStateChanged } from "firebase/Auth";
+
+import {
+  addWebPin,
+  getWebPins,
+  removeWebPin,
+  selectWebPin,
+} from "./WebpinSlice";
+
+import { useDispatch, useSelector } from "react-redux";
+
+const WebPin=()=>{
+    const dispatch=useDispatch();
+    const dispatchRef=useRef(dispatch);
+    const [webPinData,setWebPinData]=useState({
+        name: "",
+        link:"https://",
+    });
+    
+    const handleChange=(event)=>{
+        event.preventDefault();
+        setwebPinData({...webPinData,[event.target.name]: event.target.value});
+    };
+    const webPins=useSelector(selectWebPin);
+     
+    const addWebPinShortcut=async(event)=>{
+        event.preventDefault();
+        dispatch(addWebPin(webPinData));
+    };
+     useEffect(() => {
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    dispatchRef.current(getWebPins());
+  }
+});
+}, [dispatchRef]);
+    return(
+        <>
+              <div
+              className="modal fade"
+              id="exampleModal"
+              tabIndex={-1}
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+              >
+               <div className="modal-dialog">
+                   <form onSubmit={addWebPinShortcut} className="modal-content">
+
+                   <div classname="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                        Add WebSite
+                    </h1>
+                    <button 
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    ></button>
+                   </div>
+                   <div className="modal-body">
+
+                   <input 
+                   name="name"
+                   value={webPinData.name}
+                   onChange={handleChange}
+                   type="text" 
+                   className="form-control"
+                   placeholder="Website title( Max length:8)"
+                   minLength={3}
+                   required
+                   />
+                   <br />
+                   <input 
+                   type="url"
+                   name="link"
+                   onChange={handleChange}
+                   className="form-control"
+                   type="url"
+                   placeholder="Website URL"
+                   required
+                   />
+                   <div className="delete">
+                    {webPinslmap((element,index)=>{
+                        return(
+                            <div key={index} className="col py-2">
+                                <label className="text-center w-50">{element.name}</label>
+                                <button
+                                    onClick={()=>{
+                                        dispatch(removeWebPin(element.name));
+                                    }}
+                                    className="btn btn-outline-danger w-50"
+                                    >Delete
+                                </button>
+                            </div>
+                        );
+                    })}
+                    </div>
+                   </div>
+              
+               <div className="modal-footer">
+                <button type="button" 
+                className="btn btn-secondary" 
+                data-bs-dismiss="modal"
+                >Cancel
+                </button>
+                <button 
+                className="btn btn-primary"
+                type="submit">
+                Add Website
+                </button>
+               </div>
+                     </form>
+               </div>
+              </div>
+
+              <div className="row py-3">
+                {webPins.map((element,index)=>{
+                    return(
+                        <div key={index} className="col-lg-2 col-sm-3 col-4">
+                               <a rel="noreferrer" target="_blank" href={element.link}>
+                                
+                               <img 
+                                className="w-100 rounded-3 border border-1 border-secondary"
+                                style={{ cursor: "pointer" }}
+                                src={`https://www.google.com/s2/favicons?sz=64&domain_url=${element.link}`}
+                                alt=""
+                                />
+                                </a>
+                                <p className="text-center">{element.name}</p>
+
+
+                        </div>
+                    );
+                })}
+                <div className="col-md-2 col-4 py-3">
+                    <img 
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    className="w-100 rounded-3  border-1 border-secondary"
+                    style={{ cursor: "pointer" }}
+                    src="https://cdn.pixabay.com/photo/2017/03/19/03/51/material-icon-2155448_640.png"
+                    alt=""
+                     />
+                     <p className="text-center">Add Website</p>
+                </div>
+              </div>
+        </>
+
+)
+}
+export default WebPin;
---
+++ dashdo/src/Features/WebPin/WebpinSlice.js
@@ -0,0 +1,91 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import {
+  doc,
+  getDocs,
+  setDoc,
+  deleteDoc,
+  getFirestore,
+  collection,
+} from "@firebase/firestore";
+import { getAuth } from "@firebase/auth";
+import app from "../../firebase";
+
+const db = getFirestore(app);
+
+const initialState = {
+  value: [],
+  saved: false,
+};
+
+export const getWebsites = createAsyncThunk("WebPin/fetch", async () => {
+  const querySnapshot = await getDocs(
+    collection(
+      db,
+      `users/${getAuth().currentUser.uid}/dashdo/WebPin/shortcut`
+    )
+  );
+
+  let websiteUpdateArray = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    websiteUpdateArray.push(doc.data());
+  });
+  return websiteUpdateArray;
+});
+
+export const addWebPin = createAsyncThunk("WebPin/add", async (website) => {
+  console.log("Inside add website");
+  try {
+    await setDoc(
+      doc(
+        db,
+        `users/${getAuth().currentUser.uid}/dashdo/WebPin/shortcut/${
+          website.name
+        }`
+      ),
+      website
+    );
+    return website;
+  } catch (error) {
+    return website;
+  }
+});
+
+export const removeWebPin = createAsyncThunk(
+  "website/remove",
+  async (name) => {
+    console.log("removing :", name);
+    await deleteDoc(
+      doc(
+        db,
+        `users/${getAuth().currentUser.uid}/dashdo/WebPin/shortcut/${name}`
+      )
+    );
+    return name;
+  }
+);
+
+export const websiteSlice = createSlice({
+  name: "WebPin",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addWebPin.fulfilled, (state, action) => {
+        state.value.push(action.payload);
+      })
+      .addCase(removeWebPin.fulfilled, (state, action) => {
+        state.value = state.value.filter(
+          (item) => item.name !== action.payload
+        );
+      })
+      .addCase(getWebPins.fulfilled, (state, action) => {
+        state.value = action.payload;
+      });
+  },
+});
+
+export const selectWebsite = (state) => state.website.value;
+
+export default WebpinSlice.reducer;
